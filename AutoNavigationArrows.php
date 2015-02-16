f7ae94367d093a060366a74312c47926ffbbd189
Grant Bartlett <grant@smswmedia.com>
2015-02-16 17:12:20 +0000

    Converted tabs to spaces

--- AutoNavigationArrows.php
+++ AutoNavigationArrows.php
@@ -22,164 +22,170 @@
 
 class AutoNavigationArrows extends WP_Widget {
 
-	protected $widgetClass = 'Auto_Navigation_Arrows';
-	protected $widgetName = 'Auto Navigation Arrows';
-	private $widgetDesc = 'This plugin will generate previous and next navigation arrows on active page';
-	private $widgetSlug = 'auto-navigation-arrows';
-	private $arrayOfPages = [];
-	private $setupLinks = [];
+    protected $widgetClass = 'Auto_Navigation_Arrows';
+    protected $widgetName = 'Auto Navigation Arrows';
+
+    private $widgetDesc = 'This plugin will generate previous and next navigation arrows on active page';
+    private $widgetSlug = 'auto-navigation-arrows';
+    private $arrayOfPages = [];
+    private $configureLinks = [];
 
 
 
-	/**
-	 * Constructor
-	 *
-	 * Specifies class name, description and instantiates the widget.
-	 *
-	 */
-	public function __construct() {
+    /**
+     * Constructor
+     *
+     * Specifies class name, description and instantiates the widget.
+     *
+     */
+    public function __construct() {
 
-		// Load plugin text domain
-		add_action( 'init', array( $this, 'widgetTextDomain' ) );
+        // Load plugin text domain
+        add_action( 'init', array( $this, 'widgetTextDomain' ) );
 
-		// Extend WP_WIDGET Constructor
-		parent::__construct(
-			$this->getWidgetClass(),
-			__( $this->getWidgetName(), $this->getWidgetSlug() ),
+        // Extend WP_WIDGET Constructor
+        parent::__construct(
+            $this->getWidgetClass(),
+            __( $this->getWidgetName(), $this->getWidgetSlug() ),
 
-			array(
-				'classname'   => $this->getWidgetSlug() . '-class',
-				'description' => __( $this->getWidgetDesc(), $this->getWidgetSlug() )
-			)
-		);
+            array(
+                'classname'   => $this->getWidgetSlug() . '-class',
+                'description' => __( $this->getWidgetDesc(), $this->getWidgetSlug() )
+            )
+        );
 
-		// @TODO: Register admin styles and scripts
+        // @TODO: Register admin styles and scripts
 
-		// @TODO: Register site styles and scripts
+        // @TODO: Register site styles and scripts
 
-		// @TODO: Refresh cached output
+        // @TODO: Refresh cached output
 
-	}
-
-
-
-
-	/**
-	 * Outputs the content of the Widget
-	 *
-	 * WordPress Widget API
-	 *
-	 * @param array $args - The array of form elements
-	 * @param array $instance - The current instance of the widget
-	 */
-	public function widget( $args, $instance ){
-
-		echo $args['before_widget'];
-
-
-		include( plugin_dir_path(__FILE__) . 'views/widget.php' );
-
-
-		echo $args['after_widget'];
-
-	}
+    }
 
 
 
 
-	/**
-	 * Processes the widgets options to be saved
-	 *
-	 * WordPress Widget API
-	 *
-	 * @param array $new_instance - The new instance of values to be generated via the update.
-	 * @param array $old_instance - The previous instane of values before the update.
-	 *
-	 * @return array
-	 */
-	public function update( $new_instance, $old_instance ){
-		$instance = $old_instance;
+    /**
+     * Outputs the content of the Widget
+     *
+     * WordPress Widget API
+     *
+     * @param array $args - The array of form elements
+     * @param array $instance - The current instance of the widget
+     */
+    public function widget( $args, $instance ){
 
-		return $instance;
-	}
+        echo $args['before_widget'];
 
 
-	/**
-	 * @param array $setupLinks
-	 */
-	public function setSetupLinks( $setupLinks ) {
-		$pageIdsArray = array();
-
-		// Take arguments set by @getArrayOfPages
-		foreach ( $this->getArrayOfPages( $setupLinks ) as $page ) {
-			$pageIdsArray[] += $page->ID;
-		}
-		$this->setupLinks = $pageIdsArray;
-	}
+        include( plugin_dir_path(__FILE__) . 'views/widget.php' );
 
 
-	/**
-	 * @return array
-	 */
-	public function getSetupLinks() {
-		return $this->setupLinks;
-	}
+        echo $args['after_widget'];
+
+    }
 
 
-	/**
-	 * Generates the admin form for the widget
-	 *
-	 * WordPress Widget API
-	 *
-	 */
-	public function form( $instance ) {
-		return $instance;
-	}
 
-	/**
-	 * @return array
-	 */
-	public function getArrayOfPages($arrayOfPages) {
-		return $this->arrayOfPages = get_pages( $arrayOfPages );
-	}
 
-	/**
-	 * Register Widget Text Domain
-	 */
-	public function widgetTextDomain() {
-		load_plugin_textdomain( $this->getWidgetSlug(), false, plugin_dir_path( __FILE__ ) . 'lang/' );
-	}
+    /**
+     * Processes the widgets options to be saved
+     *
+     * WordPress Widget API
+     *
+     * @param array $new_instance - The new instance of values to be generated via the update.
+     * @param array $old_instance - The previous instane of values before the update.
+     *
+     * @return array
+     */
+    public function update( $new_instance, $old_instance ){
+        $instance = $old_instance;
 
-	/**
-	 * Widget Name
-	 * @return string
-	 */
-	public function getWidgetName() {
-		return $this->widgetName;
-	}
+        return $instance;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getWidgetClass() {
-		return $this->widgetClass;
-	}
 
-	/**
-	 * Widget Description
-	 * @return string
-	 */
-	public function getWidgetDesc() {
-		return $this->widgetDesc;
-	}
 
-	/**
-	 * Widget Slug
-	 * @return string
-	 */
-	public function getWidgetSlug() {
-		return $this->widgetSlug;
-	}
+
+
+    /**
+     * Generates the admin form for the widget
+     *
+     * WordPress Widget API
+     *
+     */
+    public function form( $instance ) {
+        return $instance;
+    }
+
+
+
+
+
+    /**
+     * Register Widget Text Domain
+     */
+    public function widgetTextDomain() {
+        load_plugin_textdomain( $this->getWidgetSlug(), false, plugin_dir_path( __FILE__ ) . 'lang/' );
+    }
+
+    /**
+     * @param array $configureLinks
+     */
+    public function setConfigureLinks( $configureLinks ) {
+        $pageIdsArray = array();
+
+        // Take arguments set by @getArrayOfPages
+        foreach ( $this->getArrayOfPages( $configureLinks ) as $page ) {
+            $pageIdsArray[] += $page->ID;
+        }
+        $this->configureLinks = $pageIdsArray;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfigureLinks() {
+        return $this->configureLinks;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayOfPages($arrayOfPages) {
+        return $this->arrayOfPages = get_pages( $arrayOfPages );
+    }
+
+    /**
+     * Widget Name
+     * @return string
+     */
+    public function getWidgetName() {
+        return $this->widgetName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getWidgetClass() {
+        return $this->widgetClass;
+    }
+
+    /**
+     * Widget Description
+     * @return string
+     */
+    public function getWidgetDesc() {
+        return $this->widgetDesc;
+    }
+
+    /**
+     * Widget Slug
+     * @return string
+     */
+    public function getWidgetSlug() {
+        return $this->widgetSlug;
+    }
 
 }
 
--- views/widget.php
+++ views/widget.php
@@ -5,31 +5,33 @@ $navArrows = new AutoNavigationArrows();
 
 // Custom page arguments
 $args = array(
-	'sort_order'   => 'ASC',
-	'sort_column'  => 'menu_order',
-	'hierarchical' => 1,
-	'exclude'      => '',
-	'include'      => '',
-	'meta_key'     => '',
-	'meta_value'   => '',
-	'authors'      => '',
-	'child_of'     => 0,
-	'parent'       => - 1,
-	'exclude_tree' => '',
-	'number'       => '',
-	'offset'       => '0',
-	'post_type'    => 'page',
-	'post_status'  => 'publish'
+    'sort_order'   => 'ASC',
+    'sort_column'  => 'menu_order',
+    'hierarchical' => 1,
+    'exclude'      => '',
+    'include'      => '',
+    'meta_key'     => '',
+    'meta_value'   => '',
+    'authors'      => '',
+    'child_of'     => 0,
+    'parent'       => - 1,
+    'exclude_tree' => '',
+    'number'       => '',
+    'offset'       => '0',
+    'post_type'    => 'page',
+    'post_status'  => 'publish'
 );
 
-$navArrows->setSetupLinks($args);
-$current = array_search(get_the_ID(), $navArrows->getSetupLinks());
+$navArrows->setConfigureLinks($args);
+$current = array_search(get_the_ID(), $navArrows->getConfigureLinks());
+
+echo $navArrows->getConfigureLinks($current);
 
 
 ?>
 
 <style>
-	.fixed-navigation {
-		display: none!important; /*annoying as hell */
-	}
+    .fixed-navigation {
+        display: none!important; /*annoying as hell */
+    }
 </style>
